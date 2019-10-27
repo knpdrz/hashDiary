8c43815a3f2b31c554aab9be73535372b238d464
Monika Ruszczyk <ruszczyk.monika@gmail.com>
2019-10-27 08:57:25 +0100

    display user added locations on a map and save them to db

--- app/src/main/java/com/nullptr/monever/MapsActivity.kt
+++ app/src/main/java/com/nullptr/monever/MapsActivity.kt
@@ -1,21 +1,29 @@
 package com.nullptr.monever
 
 import android.app.Activity
+import android.content.ContentValues
 import android.content.Intent
 import android.os.Bundle
+import android.provider.BaseColumns
 import android.view.View
 import android.widget.ImageView
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.setPadding
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.MarkerOptions
 import com.sucho.placepicker.AddressData
 import com.sucho.placepicker.Constants.ADDRESS_INTENT
+import com.sucho.placepicker.Constants.DEFAULT_ZOOM
 import com.sucho.placepicker.Constants.PLACE_PICKER_REQUEST
 import com.sucho.placepicker.MapType
 import com.sucho.placepicker.PlacePicker
+import kotlinx.android.synthetic.main.activity_maps.*
 import java.util.logging.Level
 import java.util.logging.Logger
 
@@ -24,6 +32,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private val logger = Logger.getLogger("MapsActivity")
     private lateinit var gMap: GoogleMap
     private lateinit var mapFragment: SupportMapFragment
+    private var userLocations = mutableListOf<LatLng>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,7 +42,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
-        prepareLocationButton()
+        addLocationButton.setOnClickListener {
+            openPlacePicker()
+        }
+    }
+
+    private fun displayUserLocations() {
+        val boundsBuilder = LatLngBounds.builder()
+        for(location in userLocations){
+            gMap.addMarker(MarkerOptions().position(location))
+            boundsBuilder.include(location)
+        }
+        val bounds = boundsBuilder.build()
+        val cameraUpdate = CameraUpdateFactory.newLatLngBounds(bounds, 50)
+        gMap.moveCamera(cameraUpdate)
     }
 
     private fun prepareLocationButton() {
@@ -42,28 +64,24 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             (mapFragment.view?.findViewById<View>(Integer.parseInt("1"))?.parent as View).findViewById<View>(
                 Integer.parseInt("2")
             ) as ImageView
-        val rlp = locationButton.layoutParams as RelativeLayout.LayoutParams
-        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0)
-        rlp.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
-        rlp.setMargins(0, 0, 100, 100)
+        val layoutParams = locationButton.layoutParams as RelativeLayout.LayoutParams
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0)
+        layoutParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE)
+        layoutParams.setMargins(0, 0, 30, 210)
+
         locationButton.background = getDrawable(R.drawable.round_button)
         locationButton.setImageDrawable(getDrawable(R.drawable.ic_my_location))
-        locationButton.setPadding(50)
+        locationButton.setPadding(40)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         gMap = googleMap
         gMap.isMyLocationEnabled = true
-        gMap.setOnMapClickListener { openPlacePicker() }
+
+        prepareLocationButton()
+
+        readUserLocationsFromDb()
+        displayUserLocations()
     }
 
     private fun openPlacePicker() {
@@ -91,9 +109,64 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             if (resultCode == Activity.RESULT_OK) {
                 val addressData = data?.getParcelableExtra<AddressData>(ADDRESS_INTENT)
                 logger.log(Level.INFO, "selected location $addressData")
+                addressData?.let { createNewUserLocation(addressData)}
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data)
         }
+    }
+
+    private fun createNewUserLocation(addressData: AddressData){
+        val newLocation = LatLng(addressData.latitude, addressData.longitude)
+        userLocations.add(newLocation)
+        saveNewLocationToDb(newLocation)
+        gMap.addMarker(MarkerOptions().position(newLocation))
+
+        val update = CameraUpdateFactory.newLatLngZoom(
+            newLocation,
+            DEFAULT_ZOOM
+        )
+        gMap.moveCamera(update)
+    }
+
+    private fun readUserLocationsFromDb() {
+        val dbHelper = LocationReaderDbHelper(applicationContext)
+        val db = dbHelper.readableDatabase
+        val projection = arrayOf(
+            BaseColumns._ID,
+            LocationReaderContract.LocationEntry.COLUMN_NAME_LAT,
+            LocationReaderContract.LocationEntry.COLUMN_NAME_LNG
+        )
+        val cursor =
+            db.query(
+                LocationReaderContract.LocationEntry.TABLE_NAME,
+                projection,
+                null,
+                null,
+                null,
+                null,
+                null
+            )
+        with(cursor) {
+            while (moveToNext()) {
+                val lat =
+                    getDouble(getColumnIndexOrThrow(LocationReaderContract.LocationEntry.COLUMN_NAME_LAT))
+                val lng =
+                    getDouble(getColumnIndexOrThrow(LocationReaderContract.LocationEntry.COLUMN_NAME_LNG))
+
+                userLocations.add(LatLng(lat, lng))
+            }
+        }
+    }
+
+    private fun saveNewLocationToDb(location: LatLng) {
+        val dbHelper = LocationReaderDbHelper(applicationContext)
+        val db = dbHelper.writableDatabase
+        val values = ContentValues().apply {
+            put(LocationReaderContract.LocationEntry.COLUMN_NAME_LAT, location.latitude)
+            put(LocationReaderContract.LocationEntry.COLUMN_NAME_LNG, location.longitude)
+        }
+        val newRowId = db?.insert(LocationReaderContract.LocationEntry.TABLE_NAME, null, values)
+        logger.log(Level.INFO, "saved location to db with id $newRowId")
     }
 }
